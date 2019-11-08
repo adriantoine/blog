4244688215f7940bb29679cfe5897626beb15212
Adrien Antoine <adriantoine@gmail.com>
2019-11-08 15:40:30 +0000

    Base homepage layout done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Avatar from "../components/avatar"
 import SEO from "../components/seo"
+import { IconGitHub, IconTwitter, IconLinkedIn } from "../components/icons"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+
+    <div className="flex flex-col items-center mt-20">
+      <div className="rounded-full overflow-hidden w-32 border-gray-800 border-4">
+        <Avatar />
+      </div>
+
+      <h1 className="mt-2">
+        Hi! I'm{" "}
+        <a className="font-bold" href="http://adriantoine.com">
+          Adrien
+        </a>
+        !
+      </h1>
+
+      <div className="flex justify-between w-40 mt-6">
+        <IconTwitter className="h-8 w-8" />
+        <IconGitHub className="h-8 w-8" />
+        <IconLinkedIn className="h-8 w-8" />
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
